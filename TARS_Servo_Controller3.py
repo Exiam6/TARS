1cd372e47b0536205a68fc56e269f9d7e861a543
Exiam6 <121872598+Exiam6@users.noreply.github.com>
2024-08-17 18:49:51 +0800

    Validated

--- TARS_Servo_Controller3.py
+++ TARS_Servo_Controller3.py
@@ -1,372 +1,175 @@
 from __future__ import division
 import time
-import Adafruit_PCA9685
+import board
+import busio
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
 from threading import Thread
 
-pwm = Adafruit_PCA9685.PCA9685()
+# Initialize I2C bus and PCA9685 on address 0x40
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+pwm = PCA9685(i2c_bus)
+pwm.frequency = 60  # Set frequency to 60hz, good for servos.
 
-# Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
-
-portMain = 610
-starMain = 200
-portForarm = 570
-starForarm = 200
-portHand = 570
-starHand = 240
-
-# Center Lift Servo (0) Values
-upHeight = 205
-neutralHeight = 275
-downHeight = 450
-
-# Port Drive Servo (1) Values
-forwardPort = 440
-neutralPort = 375
-backPort = 330
-
-# Starboard Drive Servo (2) Values
-forwardStarboard = 292
-neutralStarboard = 357
-backStarboard = 402
+servo_0 = servo.Servo(pwm.channels[0])
+servo_1 = servo.Servo(pwm.channels[1])
+servo_2 = servo.Servo(pwm.channels[2])
+angles = {
+    'default': 90,
+    'neutralHeight': 90,
+    'upHeight': 120,
+    'downHeight': 60,
+    'neutralPort': 90,
+    'forwardPort': 90,
+    'backPort': 60,
+    'neutralStarboard': 90,
+    'forwardStarboard': 120,
+    'backStarboard': 60
+}
 
 
-# moves the torso from a neutral position upwards, allowing the torso to pivot forwards or backwards
+# Moves the torso from a neutral position upwards
 def height_neutral_to_up():
-    height = neutralHeight
-    print('setting center servo (0) Neutral --> Up position')
-    while (height > upHeight):
-        height = height - 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.001)
-    print('center servo (0) set to: Up position\n ')
+    print('Setting center servo (0) from Neutral to Up position')
+    try:
+        while servo_0.angle > angles['upHeight']:
+            servo_0.angle -= 1
+            time.sleep(0.001)
+    except:
+        return
+    print('Center servo (0) set to Up position\n')
 
 
-# rotates the torso outwards, enough so that when TARS pivots and lands, the bottom of the torso is
-# flush with the ground. Making the torso flush with the ground is an intentional improvement from
-# previous programs, where TARS would land and then slide a little on smooth surfaces, which while
-# allowing for a simple walking program, inhibited TARS' ability to walk on surfaces with different
-# coefficients of friction
+# Rotates the torso outwards to pivot and land flush with the ground
 def torso_neutral_to_forwards():
-    port = neutralPort
-    starboard = neutralStarboard
-    print('setting port and starboard servos (1)(2) Neutral --> Forward')
-    while (port < forwardPort):
-        port = port + 1
-        starboard = starboard - 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
-        time.sleep(0.0001)
-    print('port and starboard servos (1)(2) set to: Forward position\n ')
+    print('Setting port and starboard servos (1)(2) from Neutral to Forward')
+
+    servo_1.angle = 120
+    servo_2.angle = 60
+
+    # time.sleep(0.0001)
+    print('Port and starboard servos (1)(2) set to Forward position\n')
 
 
+# Rotates the torso backwards to pivot and land
 def torso_neutral_to_backwards():
-    port = neutralPort
-    starboard = neutralStarboard
-    print('setting port and starboard servos (1)(2) Neutral --> Forward')
-    while (port > backPort):
-        port = port - 1
-        starboard = starboard + 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
+    print('Setting port and starboard servos (1)(2) from Neutral to Backward')
+    while servo_1.angle > angles['backPort']:
+        servo_1.angle -= 1
+        servo_2.angle += 1
         time.sleep(0.0001)
-    print('port and starboard servos (1)(2) set to: Forward position\n ')
+    print('Port and starboard servos (1)(2) set to Backward position\n')
 
 
-# rapidly shifts the torso height from UP --> DOWN and then returns --> UP, which should cause TARS
-# to pivot and land on it's torso
+# Rapidly shifts the torso height up and down, causing TARS to pivot
 def torso_bump():
-    height = upHeight
-    print('performing a torso bump\nsetting center servo (0) Up --> Down position FAST')
-    while (height < downHeight):
-        height = height + 2
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.000001)
-    print('setting center servo (0) Down --> Up position FAST')
-    while (height > upHeight):
-        height = height - 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.0001)
-    print('center servo (0) returned to Up position\n')
+    print('Performing a torso bump')
+    print('Setting center servo (0) Up to Down position FAST')
+    try:
+        while servo_0.angle < angles['downHeight']:
+            servo_0.angle += 2
+            time.sleep(0.000001)
+        print('Setting center servo (0) Down to Up position FAST')
+        while servo_0.angle > angles['upHeight']:
+            servo_0.angle -= 1
+            time.sleep(0.0001)
+        print('Center servo (0) returned to Up position\n')
+    except:
+        return
 
 
-# returns the torso's vertical height and rotation to centered values from up height and forward
-# rotation. Activates two external functions so movement in both axes can occur in parallel.
+# Returns the torso to a neutral position
 def torso_return():
     t1 = Thread(target=torso_return_rotation)
     t2 = Thread(target=torso_return_vertical)
-
     t1.start()
     t2.start()
 
 
-# returns torso's rotation to neutral from forward
+# Returns torso's rotation to neutral from forward
 def torso_return_rotation():
-    port = forwardPort
-    starboard = forwardStarboard
-    print('setting port and starboard servos (1)(2) Forward --> Neutral position')
-    while (port > neutralPort):
-        port = port - 1
-        starboard = starboard + 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
+    print('Setting port and starboard servos (1)(2) from Forward to Neutral position')
+    while servo_1.angle > angles['neutralPort']:
+        servo_1.angle -= 1
+        servo_2.angle += 1
         time.sleep(0.005)
-    print('port and starboard servos (1)(2) set to: Neutral position\n ')
+    print('Port and starboard servos (1)(2) set to Neutral position\n')
 
 
-# returns torso's vertical to neutral from up
+# Returns torso's vertical height to neutral from up
 def torso_return_vertical():
-    height = upHeight
-    print('setting center servo (0) Up --> Down position')
-    # moving the torso down to create clearance for the rotation of the legs
-    while (height < downHeight):
-        height = height + 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.00005)
-    # moving the torso up from down to neutral
-    # time.sleep(.2)
-    while (height > neutralHeight):
-        height = height - 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.00001)
-    print('center servo (0) set to: Neutral position\n ')
+    print('Setting center servo (0) from Up to Neutral position')
+    try:
+        while servo_0.angle < angles['downHeight']:
+            servo_0.angle += 1
+            time.sleep(0.00005)
+        while servo_0.angle > angles['neutralHeight']:
+            servo_0.angle -= 1
+            time.sleep(0.00001)
+        print('Center servo (0) set to Neutral position\n')
+    except:
+        return
 
 
-def torso_return2():
-    t1 = Thread(target=torso_return_rotation2)
-    t2 = Thread(target=torso_return_vertical2)
-
-    t1.start()
-    t2.start()
-
-
-# returns torso's rotation to neutral from forward
-def torso_return_rotation2():
-    port = backPort
-    starboard = backStarboard
-    print('setting port and starboard servos (1)(2) Forward --> Neutral position')
-    while (port < neutralPort):
-        port = port + 1
-        starboard = starboard - 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
-        time.sleep(0.01)
-    print('port and starboard servos (1)(2) set to: Neutral position\n ')
-
-
-# returns torso's vertical to neutral from up
-def torso_return_vertical2():
-    height = upHeight
-    print('setting center servo (0) Up --> Down position')
-    # moving the torso down to create clearance for the rotation of the legs
-    while (height < downHeight):
-        height = height + 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.001)
-    # moving the torso up from down to neutral
-    time.sleep(.25)
-    while (height > neutralHeight):
-        height = height - 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.001)
-    print('center servo (0) set to: Neutral position\n ')
-
-
-# moves the torso from neutral position to down
+# Moves the torso from neutral position to down
 def neutral_to_down():
-    height = neutralHeight
-    print('setting center servo (0) Neutral --> Down position')
-    while (height < downHeight):
-        height = height + 1
-        pwm.set_pwm(0, 0, height)
+    print('Setting center servo (0) from Neutral to Down position')
+    while servo_0.angle < angles['downHeight']:
+        servo_0.angle += 1
         time.sleep(0.001)
 
 
+# Moves the torso from down to up
 def down_to_up():
-    height = downHeight
-    print('setting center servo (0) Down --> Neutral position')
-    while (height > upHeight):
-        height = height - 1
-        pwm.set_pwm(0, 0, height)
+    print('Setting center servo (0) from Down to Up position')
+    while servo_0.angle > angles['upHeight']:
+        servo_0.angle -= 1
         time.sleep(0.001)
 
 
+# Moves the torso from down to neutral
 def down_to_neutral():
-    height = downHeight
-    print('setting center servo (0) Down --> Neutral position')
-    while (height > neutralHeight):
-        height = height - 1
-        pwm.set_pwm(0, 0, height)
+    print('Setting center servo (0) from Down to Neutral position')
+    while servo_0.angle > angles['neutralHeight']:
+        servo_0.angle -= 1
         time.sleep(0.001)
 
 
-def neutral_to_down():
-    height = neutralHeight
-    print('setting center servo (0) Down --> Neutral position')
-    while (height < downHeight):
-        height = height + 1
-        pwm.set_pwm(0, 0, height)
-        time.sleep(0.001)
-
-
+# Turns the torso to the right
 def turn_right():
-    port = neutralPort
-    starboard = neutralStarboard
-    while (port < forwardPort):
-        port = port + 1
-        starboard = starboard + 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
+    print('Turning right')
+    while servo_1.angle < angles['forwardPort']:
+        servo_1.angle += 1
+        servo_2.angle += 1
         time.sleep(0.001)
 
 
+# Turns the torso to the left
 def turn_left():
-    port = neutralPort
-    starboard = neutralStarboard
-    while (port > backPort):
-        port = port - 1
-        starboard = starboard - 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
+    print('Turning left')
+    while servo_1.angle > angles['backPort']:
+        servo_1.angle -= 1
+        servo_2.angle -= 1
         time.sleep(0.001)
 
 
+# Returns the torso to neutral from the right
 def neutral_from_right():
-    port = forwardPort
-    starboard = backStarboard
-    while (port > neutralPort):
-        port = port - 1
-        starboard = starboard - 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
+    print('Returning from right to neutral')
+    while servo_1.angle > angles['neutralPort']:
+        servo_1.angle -= 1
+        servo_2.angle -= 1
         time.sleep(0.005)
-    pwm.set_pwm(1, 1, neutralPort)
-    pwm.set_pwm(2, 2, neutralStarboard)
+    servo_1.angle = angles['neutralPort']
+    servo_2.angle = angles['neutralStarboard']
 
 
+# Returns the torso to neutral from the left
 def neutral_from_left():
-    port = backPort
-    starboard = forwardStarboard
-    while (port < neutralPort):
-        port = port + 1
-        starboard = starboard + 1
-        pwm.set_pwm(1, 1, port)
-        pwm.set_pwm(2, 2, starboard)
+    print('Returning from left to neutral')
+    while servo_1.angle < angles['neutralPort']:
+        servo_1.angle += 1
+        servo_2.angle += 1
         time.sleep(0.005)
-    pwm.set_pwm(1, 1, neutralPort)
-    pwm.set_pwm(2, 2, neutralStarboard)
-
-
-# Arm shenanigans
-# port Main Arm
-def portMainPlus():
-    global portMain
-    portMain = portMain - 10
-    pwm.set_pwm(3, 3, portMain)
-    time.sleep(0.0001)
-    print("increase starMain")
-    print(portMain)
-
-
-def portMainMinus():
-    global portMain
-    portMain = portMain + 10
-    pwm.set_pwm(3, 3, portMain)
-    time.sleep(0.0001)
-    print("decrease starMain")
-    print(portMain)
-
-
-# port Forarm
-def portForarmPlus():
-    global portForarm
-    portForarm = portForarm - 10
-    pwm.set_pwm(4, 4, portForarm)
-    time.sleep(0.0001)
-    print("increase starForarm")
-    print(portForarm)
-
-
-def portForarmMinus():
-    global portForarm
-    portForarm = portForarm + 10
-    pwm.set_pwm(4, 4, portForarm)
-    time.sleep(0.0001)
-    print("decrease starForarm")
-    print(portForarm)
-
-
-# port Hand
-def portHandPlus():
-    global portHand
-    portHand = portHand - 10
-    pwm.set_pwm(5, 5, portHand)
-    time.sleep(0.0001)
-    print("increase starHand")
-    print(portHand)
-
-
-def portHandMinus():
-    global portHand
-    portHand = portHand + 10
-    pwm.set_pwm(5, 5, portHand)
-    time.sleep(0.0001)
-    print("decrease starHand")
-    print(portHand)
-
-
-# starboard Main Arm
-def starMainPlus():
-    global starMain
-    starMain = starMain + 10
-    pwm.set_pwm(6, 6, starMain)
-    time.sleep(0.0001)
-    print("increase starMain")
-    print(starMain)
-
-
-def starMainMinus():
-    global starMain
-    starMain = starMain - 10
-    pwm.set_pwm(6, 6, starMain)
-    time.sleep(0.0001)
-    print("decrease starMain")
-    print(starMain)
-
-
-# port Forarm
-def starForarmPlus():
-    global starForarm
-    starForarm = starForarm + 10
-    pwm.set_pwm(7, 7, starForarm)
-    time.sleep(0.0001)
-    print("increase starForarm")
-    print(starForarm)
-
-
-def starForarmMinus():
-    global starForarm
-    starForarm = starForarm - 10
-    pwm.set_pwm(7, 7, starForarm)
-    time.sleep(0.0001)
-    print("decrease starForarm")
-    print(starForarm)
-
-
-# port Hand
-def starHandPlus():
-    global starHand
-    starHand = starHand + 10
-    pwm.set_pwm(8, 8, starHand)
-    time.sleep(0.0001)
-    print("increase starHand")
-    print(starHand)
-
-
-def starHandMinus():
-    global starHand
-    starHand = starHand - 10
-    pwm.set_pwm(8, 8, starHand)
-    time.sleep(0.0001)
-    print("decrease starHand")
-    print(starHand)
+    servo_1.angle = angles['neutralPort']
+    servo_2.angle = angles['neutralStarboard']
--- main.py
+++ main.py
@@ -1,58 +1,124 @@
-import TARS_Servo_Abstractor3
-import TARS_Servo_Controller3
+import requests
+import os
+import time
+import threading
+from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 import board
 import busio
 
-
-# Initialize I2C bus
+# Initialize I2C bus and PCA9685
 i2c_bus = busio.I2C(board.SCL, board.SDA)
-
-# Initialize PCA9685 on address 0x40
 pwm = PCA9685(i2c_bus)
 pwm.frequency = 60
 
-#States
-IDLE = 0
-PORT_MAIN_PLUS = 1
-PORT_MAIN_MINUS = 2
-STAR_MAIN_PLUS = 3
-STAR_MAIN_MINUS = 4
-STEP_FORWARD = 5
-TURN_RIGHT = 6
-TURN_LEFT = 7
-POSE = 8
-UNPOSE = 9
+# Initialize servos
+servo_0 = servo.Servo(pwm.channels[0])
+servo_1 = servo.Servo(pwm.channels[1])
+servo_2 = servo.Servo(pwm.channels[2])
 
+# States
+IDLE = 0
+STEP_FORWARD = 1
+TURN_RIGHT = 2
+TURN_LEFT = 3
+POSE = 4
+UNPOSE = 5
+
+# Current state
 state = IDLE
+
+
+# Function to record audio using arecord
+def record(MIC_INDEX=0, DURATION=5):
+    '''
+    Record audio using the specified microphone.
+    MIC_INDEX: Microphone ID (use `arecord -l` to find the ID)
+    DURATION: Duration of the recording in seconds
+    '''
+    print(f'Starting {DURATION} seconds of recording...')
+    os.system(f'sudo arecord -D "plughw:{MIC_INDEX}" -f dat -c 1 -r 16000 -d {DURATION} temp/speech_record.wav')
+    print('Recording finished')
+
+
+# Function to play audio on Raspberry Pi
+def play_audio(filename):
+    os.system(f'aplay {filename}')
+
+
+# Intranet Penetration
+upload_url = 'https://7d9c-202-66-60-166.ngrok-free.app/upload'
+download_url = 'https://7d9c-202-66-60-166.ngrok-free.app/download'
+
+# File paths
+input_file = 'temp/speech_record.wav'
+response_file = 'response.wav'
+
+
+def perform_action(action):
+    global state
+    if action == STEP_FORWARD:
+        state = STEP_FORWARD
+        TARS_Servo_Abstractor3.stepForward()
+    elif action == TURN_RIGHT:
+        state = TURN_RIGHT
+        TARS_Servo_Abstractor3.turnRight()
+    elif action == TURN_LEFT:
+        state = TURN_LEFT
+        TARS_Servo_Abstractor3.turnLeft()
+    elif action == POSE:
+        state = POSE
+        TARS_Servo_Abstractor3.pose()
+    elif action == UNPOSE:
+        state = UNPOSE
+        TARS_Servo_Abstractor3.unpose()
+    else:
+        state = IDLE
+        print("Idle state, waiting for command...")
+
 
 def main():
     global state
-    print("Starting servo control...")
+    global servo_0, servo_1, servo_2
+    print("TARS awaken...")
 
     while True:
-        if state == PORT_MAIN_PLUS:
-            TARS_Servo_Controller3.portMainPlus()
-        elif state == PORT_MAIN_MINUS:
-            TARS_Servo_Controller3.portMainMinus()
-        elif state == STAR_MAIN_PLUS:
-            TARS_Servo_Controller3.starMainPlus()
-        elif state == STAR_MAIN_MINUS:
-            TARS_Servo_Controller3.starMainMinus()
-        elif state == STEP_FORWARD:
-            TARS_Servo_Abstractor3.stepForward()
-        elif state == TURN_RIGHT:
-            TARS_Servo_Abstractor3.turnRight()
-        elif state == TURN_LEFT:
-            TARS_Servo_Abstractor3.turnLeft()
-        elif state == POSE:
-            TARS_Servo_Abstractor3.pose()
-        elif state == UNPOSE:
-            TARS_Servo_Abstractor3.unpose()
-        else:
-            print("Idle state, waiting for command...")
+        # Record audio from microphone using arecord
+        record(MIC_INDEX=0, DURATION=5)
 
-        time.sleep(1)  # Delay for 1 second between checks
+        # Upload audio to the server
+        with open(input_file, 'rb') as file:
+            files = {'file': file}
+            response = requests.post(upload_url, files=files, verify=False)
+
+        # Handle the response
+        if response.status_code == 200:
+            json_response = response.json()
+            print("Server response:", json_response['response'])
+
+            filename = json_response['file_url']
+            file_response = requests.post(download_url, json={'filename': filename}, verify=False)
+
+            with open(response_file, 'wb') as f:
+                f.write(file_response.content)
+
+            action = json_response.get('action', IDLE)
+
+            # Create threads for action and audio playback simultaneously
+            action_thread = threading.Thread(target=perform_action, args=(action,))
+            audio_thread = threading.Thread(target=play_audio, args=(response_file,))
+
+            action_thread.start()
+            audio_thread.start()
+
+            action_thread.join()
+            audio_thread.join()
+
+        else:
+            print(f"Failed to upload audio. Status code: {response.status_code}")
+
+        time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
